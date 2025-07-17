ee90a828fe8cdf89bd8524570c211d6bb6934965
Thang Hua <th8z@mtmail.mtsu.edu>
2025-07-16 21:33:35 -0500

    added subprocessor to exe uMd(built) Also added moku control function to adjust hz over a period of 40 seconds

--- PYbridge/mqtt.py
+++ PYbridge/mqtt.py
@@ -1,14 +1,34 @@
 
 import time
+import threading
+import subprocess
 import paho.mqtt.client as mqtt
+from moku.instruments import WaveformGenerator
+
+
+
 
 class Mqtt():
-    def __init__(self): #controller
-    
+    def __init__(self): 
+        # Full path to your compiled VB.NET executable
+        exe_path = r"C:\Users\thang\source\repos\moku_projects\uMD_GUI-30-Dec-7.29PM-20250316T042754Z-001\uMD_GUI-30-Dec-7.29PM\bin\Debug\uMD_GUI.exe"
+        # Launch the .exe
+        subprocess.Popen(exe_path)
+
         print("Python Side")
         self.cli = mqtt.Client()
         self.cli.on_connect, self.cli.on_message = self.on_connect, self.on_message
         self.cli.connect("localhost", 1883, 60)
+
+        # Initialize Moku (Replace with your IP)
+        self.moku_ip = "192.168.137.66"  #########Change this to your Moku's IP address
+        if self.moku_ip == True:
+            print("we found somthing")
+
+        self.inst = WaveformGenerator(self.moku_ip)
+        self.inst.set_defaults()
+
+        #loop forever
         self.cli.loop_forever()
 
     def delay_search(self):
@@ -31,7 +51,16 @@ class Mqtt():
         print(msg)
         data = msg.payload.decode()
         print(data)
-    
+        
+        if data.strip().lower() == "start_waveform":
+            threading.Thread(target=self.run_waveform_sequence).start()
+
+    def run_waveform_sequence(self):
+        print("Setting waveform: 1 Hz, 5 Vpp")
+        self.inst.generate_waveform(channel=1, type='Sine', amplitude=2.5, frequency=1, offset=0.0)
+        time.sleep(20)
+        print("Switching waveform: 10 Hz, 5 Vpp")
+        self.inst.generate_waveform(channel=1, type='Sine', amplitude=2.5, frequency=10, offset=0.0)
 
 
 if __name__=="__main__":
