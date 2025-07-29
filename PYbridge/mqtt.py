5ac78bdb210b4a944f83cfd0f017c653f0a87ea8
Thang Hua <th8z@mtmail.mtsu.edu>
2025-07-29 18:02:30 -0500

    added data into csv and improved voltage to 5

--- PYbridge/mqtt.py
+++ PYbridge/mqtt.py
@@ -3,9 +3,6 @@ import subprocess #to run the VB.NET executable
 import paho.mqtt.client as mqtt
 
 
-
-
-
 class Mqtt():
     def __init__(self): 
         # Full path to your compiled VB.NET executable
@@ -13,29 +10,35 @@ class Mqtt():
         # Launch the .exe
         subprocess.Popen(exe_path)
 
+        self.value = 0.0  # Initialize value to 0.0
+
         print("Python Side")
         self.cli = mqtt.Client()
         self.cli.on_connect, self.cli.on_message = self.on_connect, self.on_message
         self.cli.connect("localhost", 1883, 60)
        
         #loop forever
-        self.cli.loop_forever()
+        self.cli.loop_start()
 
     def on_connect(self,cli, ud, flg, rc):
     
         connect = cli.subscribe("vb_to_py")
-        if connect == 0:
-            print("Python connected")
+        if connect:
+            print("Python connected to mqqt topic")
+            
 
 
     def on_message(self, cli, ud, msg): #unloads what vb bridge sends -> then use it
         data = msg.payload.decode()
-
+        
         #check if data is numeric
         try:
             num = float(data)
-            print(f"encoded data: {num}\n") #shows the numeric data received for checking
-            
+            #print(f"encoded data: {num}\n") #shows the numeric data received for checking
+            self.value = num
         except ValueError:
             print("Received non-numeric data, ignoring...")
 
+    def latest_value(self):
+        return self.value  # Return the latest numeric value received
+        
--- PYbridge/readme.txt
+++ PYbridge/readme.txt
@@ -1,6 +1,7 @@
 ﻿moku IP: 192.168.73.1
 
-📡 Future Improvements
+
+------- Future Improvements
 
 Add waveform parameter customization (frequency, duration, amplitude)
 
@@ -9,3 +10,22 @@ Implement graceful disconnection using inst.close()
 Support multiple commands or waveform types
 
 create a main to run : mqtt, gui for pid, voltage control
+
+------- Notes
+#get the data from mqtt and put it into a text file
+        #write to a csv file
+        with open("data.csv", "w") as csv:
+            csv.write("value, time\n")
+
+            while count <= 5000:
+                value = mqtt.latest_value()  # Get the latest value from MQTT
+                count += 1
+                if value is not None:
+                    #if numbers are getting read, put into csv
+                    csv.write(f"{value}, {count}\n")
+                    print(f"{value}")
+
+        #parse csv data
+        df = pd.read_csv("data.csv", header=0)
+        df["displacement"] = df["value"].diff().fillna(0)  # Calculate displacement
+        df.to_csv("data.csv", index=False) #replaces original csv with new data
