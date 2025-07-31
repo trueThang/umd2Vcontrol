39c15cd4b12778a3eb05286b1e31f9978ea3e4af
Thang Hua <th8z@mtmail.mtsu.edu>
2025-07-31 12:07:45 -0500

    implemented gradient and threshhold

--- PYbridge/main.py
+++ PYbridge/main.py
@@ -1,8 +1,11 @@
 ﻿
 import os
+from pickle import INT
 import sys
 import time
 import signal
+import statistics as stats
+import numpy as np
 import threading as thread
 from tracemalloc import stop
 import pandas as pd
@@ -42,9 +45,14 @@ def main():
             raise Exception("Failed to initialize MQTT or Moku connection.")
         
         initial_vpp = moku.set_voltage(5)
+        curr_vpp = initial_vpp
         file = "data.csv" #ending in .csv
         status = True #for readbility
         csv.open_file(file)
+        prev_data = 0.0
+        THRESHOLD = 0
+        INTERVAL = 50
+        disp_points = np.array([])
 
         while not stop_flag: #loop until stop_flag is set
 
@@ -63,7 +71,32 @@ def main():
             
             #put data into csv name file
             csv.write_file(file, data)
-            print(data)
+            
+            
+            
+            disp_points.append(data)  # add data to array
+
+            # adjusting length to be less than interval
+            if (len(disp_points > INTERVAL)):
+                disp_points = disp_points[1:]
+            
+            grad = np.gradient(disp_points) # get gradient of current points charted (at interval)
+            print(f'Current gradient: {grad}, Threshold: {THRESHOLD}, Interval {INTERVAL}')
+
+
+            if grad > THRESHOLD :
+
+                # could be flawed for adjustment 
+                if (grad > 0):
+                    moku.set_voltage(curr_vpp := curr_vpp - 1.0)
+                else:
+                    moku.set_voltage(curr_vpp := curr_vpp + 1.0)
+                
+
+            
+
+                
+
 
 
         #plot the data
